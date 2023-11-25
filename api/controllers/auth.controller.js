7465384aff20bd7aaee1f32c49dc3108383b398f
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2023-11-25 17:00:26 +0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
@@ -19,4 +20,27 @@ export const signup = async (req, res, next) => {
         next(error);
     }
     
-}
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong password'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        
+        const expiryDate = new Date(Date.now() + 360000); //1 hour
+        res
+        .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+};
