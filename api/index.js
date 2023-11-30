33f222752d8f7f9e9969d63a739a133c18495c44
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2023-11-30 20:51:51 +0700

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
@@ -10,6 +11,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 mongoose.connect(process.env.MONGO, {
     writeConcern: { w: 'majority' },
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,12 +2,15 @@ import express from 'express';
 import { 
     deleteUser, 
     getAll, 
-    getById 
+    getById, 
+    updateUser
 } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/', getAll);
+router.post('/update/:id', verifyToken, updateUser);
 router.get('/:id', getById);
 router.delete('/:id', deleteUser);
 
